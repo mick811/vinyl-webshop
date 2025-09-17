a4ee262445f3061b8d9f6559c40f77b6bec882cd
mick811 <mikkelelvers@outlook.com>
2025-09-17 18:12:36 +0200

    feat: setup basic frontend

---
+++ apps/site/src/routes/__root.tsx
@@ -0,0 +1,49 @@
+import {
+	createRootRoute,
+	HeadContent,
+	Outlet,
+	Scripts,
+} from "@tanstack/react-router";
+import type { ReactNode } from "react";
+import styles from "@/index.css?url";
+
+export const Route = createRootRoute({
+	head: () => ({
+		meta: [
+			{
+				charSet: "utf-8",
+			},
+			{
+				name: "viewport",
+				content: "width=device-width, initial-scale=1",
+			},
+			{
+				title: "Vinyl Webshop",
+			},
+		],
+		links: [{ rel: "stylesheet", href: styles }],
+	}),
+	component: RootComponent,
+});
+
+function RootComponent() {
+	return (
+		<RootDocument>
+			<Outlet />
+		</RootDocument>
+	);
+}
+
+function RootDocument({ children }: { children: ReactNode }) {
+	return (
+		<html lang="en">
+			<head>
+				<HeadContent />
+			</head>
+			<body>
+				{children}
+				<Scripts />
+			</body>
+		</html>
+	);
+}
