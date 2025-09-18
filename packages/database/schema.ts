1a3e3584430f026c0346d0b8712067ab8b92364a
mick811 <mikkelelvers@outlook.com>
2025-09-18 08:13:19 +0200

    fix: biome fixing errors

--- packages/database/schema.ts
+++ packages/database/schema.ts
@@ -1,10 +1,5 @@
 import { sql } from "drizzle-orm";
-import { 
-	integer,
-	pgEnum,
-	pgTableCreator,
-	text,
-} from "drizzle-orm/pg-core";
+import { integer, pgTableCreator, text } from "drizzle-orm/pg-core";
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -22,12 +17,36 @@ export const products = createTable("products", {
 	genre: text("genre").notNull(),
 	price: integer("price").notNull(),
 	image_uri: text("image_uri").notNull(),
-	media_condition: text("media_condition", { 
-		enum: ["mint", "near_mint", "very_good_plus", "very_good", "good_plus", "good", "fair", "poor"] 
-	  }).notNull(),
-	sleeve_condition: text("sleeve_condition", {
-		enum: ["mint", "near_mint", "very_good_plus", "very_good", "good_plus", "good", "fair", "poor", "generic", "no_cover"]
+	media_condition: text("media_condition", {
+		enum: [
+			"mint",
+			"near_mint",
+			"very_good_plus",
+			"very_good",
+			"good_plus",
+			"good",
+			"fair",
+			"poor",
+		],
 	}).notNull(),
-	created_at: integer("created_at").notNull().default(sql`extract(epoch from now())`),
-	updated_at: integer("updated_at").notNull().default(sql`extract(epoch from now())`),
+	sleeve_condition: text("sleeve_condition", {
+		enum: [
+			"mint",
+			"near_mint",
+			"very_good_plus",
+			"very_good",
+			"good_plus",
+			"good",
+			"fair",
+			"poor",
+			"generic",
+			"no_cover",
+		],
+	}).notNull(),
+	created_at: integer("created_at")
+		.notNull()
+		.default(sql`extract(epoch from now())`),
+	updated_at: integer("updated_at")
+		.notNull()
+		.default(sql`extract(epoch from now())`),
 });
