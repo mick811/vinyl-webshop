45b3477dd89176a3b6163d97b85eaa44278d4b0d
mick811 <mikkelelvers@outlook.com>
2025-09-19 09:08:55 +0200

    fix: made landing page fixed

--- apps/site/src/routes/index.tsx
+++ apps/site/src/routes/index.tsx
@@ -8,48 +8,48 @@ export const Route = createFileRoute("/")({
 });
 
 function RouteComponent() {
-	return (
-		<main className="min-h-screen flex items-center justify-center overflow-hidden select-none pb-[10vh] md:pb-0">
-			<div className="absolute inset-0 -z-10">
-				<GrainGradient
-					className="w-full h-full"
-					softness={0.89}
-					intensity={0.5}
-					noise={0}
-					shape="corners"
-					offsetX={0}
-					offsetY={0}
-					scale={0.76}
-					rotation={0}
-					speed={1.8}
-					colors={["#C0E916", "#8CDD0C", "#59D102"]}
-				/>
-			</div>
-			<div className="absolute inset-0 -z-10 bg-foreground/20" />
-
-			<section className="px-6">
-				<h1 className="text-secondary text-center text-balance font-normal tracking-tight text-8xl md:text-9xl font-serif leading-none">
-					<span className="italic">passion</span> for vinyl
-				</h1>
-
-				<p className="text-secondary text-center text-balance font-normal tracking-tight text-xl md:text-2xl font-lora leading-none mt-4 max-w-xl mx-auto">
-					Vinylplader er mere end bare musik. De er et stykke historie, et
-					stykke kunst, et stykke kultur.
-				</p>
-
-				<div className="flex justify-center mt-8 items-center">
-					<Link to="/">
-						<Button
-							variant="default"
-							className="cursor-pointer inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-gradient-to-r from-secondary to-secondary/90 text-foreground"
-							size="lg"
-						>
-							Udforsk vinyler
-							<ArrowRightIcon className="w-4 h-4" />
-						</Button>
-					</Link>
+	return (	
+			<main className="fixed inset-0 w-full h-full flex items-center justify-center overflow-hidden select-none">
+				<div className="absolute inset-0 -z-10">
+					<GrainGradient
+						className="w-full h-full"
+						softness={0.89}
+						intensity={0.5}
+						noise={0}
+						shape="corners"
+						offsetX={0}
+						offsetY={0}
+						scale={0.76}
+						rotation={0}
+						speed={1.8}
+						colors={["#C0E916", "#8CDD0C", "#59D102"]}
+					/>
 				</div>
-			</section>
-		</main>
+				<div className="absolute inset-0 -z-10 bg-foreground/20" />
+
+				<section className="px-6">
+					<h1 className="text-secondary text-center text-balance font-normal tracking-tight text-8xl md:text-9xl font-serif leading-none">
+						<span className="italic">passion</span> for vinyl
+					</h1>
+
+					<p className="text-secondary text-center text-balance font-normal tracking-tight text-xl md:text-2xl font-lora leading-none mt-4 max-w-xl mx-auto">
+						Vinylplader er mere end bare musik. De er et stykke historie, et
+						stykke kunst, et stykke kultur.
+					</p>
+
+					<div className="flex justify-center mt-8 items-center">
+						<Link to="/">
+							<Button
+								variant="default"
+								className="cursor-pointer inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-gradient-to-r from-secondary to-secondary/90 text-foreground"
+								size="lg"
+							>
+								Udforsk vinyler
+								<ArrowRightIcon className="w-4 h-4" />
+							</Button>
+						</Link>
+					</div>
+				</section>
+			</main>
 	);
 }
